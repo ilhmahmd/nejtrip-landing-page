36f3f466df31c5b2585b1f22393309d755dfe349
ilhmahmd <ilhamahmadmau@gmail.com>
2025-08-23 14:07:58 +0700

    add slug in trip detail fix

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function App() {
                 </>
               }
             />
-            <Route path="/trip/:id" element={<TripDetail />} />
+            <Route path="/trip/:slug" element={<TripDetail />} />
             <Route path="/about" element={<About />} />
             <Route path="/syaket" element={<Syaket />} />
             <Route path="/destinasi/:id" element={<DestinasiDetail />} />
