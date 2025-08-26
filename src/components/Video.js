8a5b066c4fbce170be467eb91d6ca35839c6cb0e
ilhmahmd <ilhamahmadmau@gmail.com>
2025-08-26 14:09:39 +0700

    change yt short and add subscribe button

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,11 @@ import { gsap } from 'gsap';
 import { useInView } from 'react-intersection-observer';
 
 const Video = () => {
-  const videoUrl = "https://youtube.com/embed/jXhYX4l_P1Y?si=xlj4c67FS_w07de5";
+  const videoUrl = "https://youtube.com/embed/qHcCa7HToko?si=B1mg7kYgZ_170Cls";
+  const subscribeUrl = "https://www.youtube.com/@nejtrip";
+  const channelName = "Nejtrip";
+  const channelProfilePic = '/images/ytlogo.png';
+
   const titleRef = useRef(null);
   const cursorRef = useRef(null);
   const text = "Trip Highlight";
@@ -61,13 +65,51 @@ const Video = () => {
       </h2>
       <div className="row justify-content-center">
         <div className="col-12 col-md-10 col-lg-8">
-          <div className="ratio ratio-16x9">
+
+          {/* YouTube Subscribe Card */}
+          <div className="card mb-4 p-3 border-0 shadow-sm mx-auto" style={{ backgroundColor: '#f8f9fa', width: 'fit-content' }}>
+            <div className="d-flex align-items-center">
+              <img
+                src={channelProfilePic}
+                alt="Channel Profile"
+                className="rounded-circle me-3"
+                style={{ width: '50px', height: '50px', objectFit: 'cover' }}
+              />
+              <h5 className="mb-0 me-3" style={{ fontFamily: '"Nunito", sans-serif', fontWeight: 700, color: '#0081a1' }}>{channelName}</h5>
+              <a
+                href={subscribeUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-danger btn-sm flex-shrink-0 p-2 pulse-animation"
+                style={{ fontFamily: '"Nunito", sans-serif', fontWeight: 700 }}
+              >
+                <i className="fab fa-youtube me-1"></i> Subscribe
+              </a>
+            </div>
+          </div>
+          {/* End of YouTube Subscribe Card */}
+
+          {/* Menyesuaikan lebar dan rasio video */}
+          <div style={{
+            position: 'relative',
+            paddingBottom: '100%', // Menggunakan paddingBottom untuk menjaga rasio 9x16
+            height: 0,
+            maxWidth: '400px', // Batasi lebar maksimum video di sini
+            margin: 'auto'
+          }}>
             <iframe
               src={videoUrl}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%'
+              }}
             ></iframe>
           </div>
         </div>
