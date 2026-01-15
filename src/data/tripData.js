7e487a85d8bdd9cd0f5bea1fcfbb4c88e4aa9ec6
ilhmahmd <ilhamahmadmau@gmail.com>
2026-01-15 21:35:38 +0700

    update itinerary and price

--- src/data/tripData.js
+++ src/data/tripData.js
@@ -168,7 +168,7 @@ export const trips = [
     // Menggunakan kata-kata deskriptif
     slug: "private-trip",
     title: "Private Trip",
-    price: "Mulai IDR. 3.300.000",
+    price: "Mulai IDR. 3.500.000",
     image: "/images/ban.jpg",
     description: "Rasakan kebebasan berpetualang dengan Private Trip 4 hari 3 malam di Banda Neira. Jadwal fleksibel dan layanan personal untuk pengalaman yang lebih intim dan sesuai keinginan Anda.",
     mepo: "Pelabuhan/Bandara Banda Neira",
@@ -177,7 +177,7 @@ export const trips = [
       { pax: "2", price: "IDR 4.500.000 /pax" },
       { pax: "3", price: "IDR 4.000.000 /pax" },
       { pax: "4", price: "IDR 3.700.000 /pax" },
-      { pax: "5", price: "IDR 3.50.000 /pax" },
+      { pax: "5", price: "IDR 3.500.000 /pax" },
     ],
     schedule: [
       ['Maret', '05 - 08', '1 orang'],
